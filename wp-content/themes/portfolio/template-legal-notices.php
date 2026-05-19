ccce81ced785cb56bee819ab63fed65e2252a032
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-05-19 22:24:43 +0200

    feat(template): add legal-noticies page template

---
+++ wp-content/themes/portfolio/template-legal-notices.php
@@ -0,0 +1,30 @@
+<?php
+// Template Name: Legal notices
+?>
+<?php get_header(); ?>
+
+<section class="notices fadeInLeft">
+
+<h2 role="heading" id="title" class="notices__title"><?= esc_html(get_field('title')) ?></h2>
+</section>
+
+<section class="notices__content fadeInLeft ">
+    <h2 role="heading" id="title-sro" class="sro">Contenu de la section légale</h2>
+
+
+    <?php if (have_rows('mentions_legales')): ?>
+
+            <?php while (have_rows('mentions_legales')) : the_row(); ?>
+
+            <article data-showup="true">
+                <h3 class="notices__sub-title"><?= esc_html(get_sub_field('sub_title')) ?></h3>
+                <div class="notices__text">
+                    <?= wp_kses_post(get_sub_field('desc')) ?>
+                </div>
+            </article>
+
+            <?php endwhile; ?>
+    <?php endif; ?>
+</section>
+
+<?php get_footer(); ?>
