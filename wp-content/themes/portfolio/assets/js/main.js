5e536b7ab6ca6f156a9b17655d6c5785eb2bd263
AlyssaBaaroun <alyssa.baaroun@student.hepl.be>
2026-05-19 22:29:36 +0200

    feat: add js

--- wp-content/themes/portfolio/assets/js/main.js
+++ wp-content/themes/portfolio/assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('Test');
+console.log('coucou je suis un test');
+
+const animation = {
+    init() {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.remove('hidden');
+                    entry.target.classList.add('showUp');
+                }
+            });
+        });
+        document.querySelectorAll('[data-showup="true"]').forEach(element => {
+            element.classList.add('hidden');
+            observer.observe(element);
+
+        });
+    },
+};
+animation.init();
